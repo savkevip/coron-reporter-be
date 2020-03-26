945d4195ffb9c9017e174b7780723e2d2baf57f1
Milos Savic <savkevip@gmail.com>
2020-03-26 11:12:10 +0100

    fix: 🐛 register

--- routes/auth.js
+++ routes/auth.js
@@ -34,7 +34,7 @@ router.post("/login", async (req, res, next) => {
 
 router.post("/register", async (req, res, next) => {
    try {
-       const { email, password, acceptedTermsAndConditions } = req.body;
+       const { details: { email, password }, acceptedTermsAndConditions } = req.body;
 
        const { error } = validate({ ...req.body }, schemaUser);
        if (error) return res.status(400).send(error.details[0].message);
